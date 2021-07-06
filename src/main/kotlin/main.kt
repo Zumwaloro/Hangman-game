f44730954d7edc7de50ca59eafc6af18643187a0
Attila Bulenda <attilb@netcompany.com>
2021-07-06 09:51:52 +0200

    Created basic structure
    Basic classes and a quick, simple menu root is added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,30 @@
 fun main(args: Array<String>) {
-    println("Hello world!")
+menu()
+}
+
+fun menu() {
+
+    println("")
+    println("")
+    println("")
+    println("")
+    println("      ,%%%%%%%%,            ___________________________________")
+    println("     %%o%%/%%%%%%")
+    println("    %%%%¨\\%%%<%%%%%           H -- A -- N -- G -- M -- A -- N ")
+    println("    %%>%%%/%%%%o%%          ___________________________________ ")
+    println("    %%%%%o%%\\%%//%")
+    println("    %\\o%\\%%/%o/%%'        'What did you bring me, my dear friends,")
+    println("     '%%\\ `%/%%%'            To keep me from the Gallows Pole?'")
+    println("       '%| |%|%'                     - Led Zeppelin -")
+    println("         | | (O")
+    println("         | | |\\                       ___ MENU:___")
+    println("         | | >>                       1 Play")
+    println("         | |                          2 HighScore")
+    println("        /   \\                         0 Quit")
+    println(" ^^^^^^^^^^^^^^^^^^^^")
+    println("")
+    println("")
+    println("")
+    println("")
+    
 }
