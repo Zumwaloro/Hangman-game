25b02d100410caad04133fdbec07e82a40607bc7
Attila Bulenda <attilb@netcompany.com>
2021-07-06 15:36:58 +0200

    Added high-scores, not sorted yet

--- src/main/kotlin/game/HighScores.kt
+++ src/main/kotlin/game/HighScores.kt
@@ -1,4 +1,29 @@
 package game
 
 class HighScores {
+    var scores = mutableListOf<GameSequence>()
+
+    //TODO: sort the highscores
+    fun SetSequence(sequence: GameSequence) {
+       scores.add(sequence)
+    }
+
+    fun DisplayHighScores() {
+        if (scores.size == 0) {
+            println("")
+            println("No high-scores yet.")
+        } else {
+            println("")
+            println("High-scores in descending order: ")
+            println("")
+            scores.forEachIndexed { index, score ->
+                println("Place: ${index+1}")
+                println("Name: ${score.name}")
+                println("Total points: ${score.totalPoints}")
+                println("Number of games: ${score.numberOfgames}")
+                println("")
+            }
+        }
+    }
+
 }
